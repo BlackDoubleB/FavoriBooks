be1056de84e053c935b010df9fd496e05e38fd81
BlackDoubleB <blancablacido@gmail.com>
2025-05-07 15:43:32 -0500

    test

--- src/app/features/auth/component/sign-in/sign-in.component.ts
+++ src/app/features/auth/component/sign-in/sign-in.component.ts
@@ -1,18 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../service/auth.service';
+import { isRequired, hasEmailError } from '../../utils/validators';
 
-interface FormSignIn{
-  email:FormControl<string | null>;
-  password:FormControl<string | null>;
+interface FormSignIn {
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
 }
 
 @Component({
   selector: 'app-sign-in',
-  imports: [RouterLink,ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.css'
+  styleUrl: './sign-in.component.css',
 })
 export default class SignInComponent {
+  private _formBuilder = inject(FormBuilder);
+  private _authService = inject(AuthService);
+  private _router = inject(RouterLink);
 
+  isRequired(field: 'email' | 'password') {
+    return isRequired(field, this.form);
+  }
+
+  hasEmailError() {
+    return hasEmailError(this.form);
+  }
+
+  form = this._formBuilder.group<FormSignIn>({
+    email: this._formBuilder.control('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: this._formBuilder.control('', Validators.required),
+  });
+
+  async submit() {
+    if (this.form.invalid) return;
+
+    try {
+      const {email, password} = this.form.value;
+      if(!email || !password) return;
+      console.log({email, password});
+
+      await this._authService.signIn({email, password});
+      toast.success("Hello, welcome back!")
+    } catch (error) {}
+  }
 }
